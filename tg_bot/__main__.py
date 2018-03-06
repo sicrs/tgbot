7bc75d01eba6b9aa7e8b2b00e9e4e300c606f1e3
sicrs <sebastiantaharuddin.personal@gmail.com>
2018-03-06 20:31:51 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -28,7 +28,7 @@ I'm built in python3, using the python-telegram-bot library.
 
 You can find the list of available commands with /help.
 
-If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
+
 """
 
 HELP_STRINGS = """
