b4dd6d47dcf7fe08475323e0a98ddaab93b424f1
automade <sebastiantaharuddin.personal@gmail.com>
2018-03-31 21:07:42 +0700

    Me nub

--- tg_bot/modules/getlink.py
+++ tg_bot/modules/getlink.py
@@ -5,6 +5,7 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 from tg_bot.modules.helper_funcs.chat_status import bot_admin
+from tg_bot.modules.helper_funcs.filters import CustomFilters
 
 from tg_bot import dispatcher
 
@@ -23,3 +24,7 @@ def getlink(bot: Bot, update: Update, args: List[int]):
         update.effective_message.reply_text(invitelink)
     else:
         update.effective_message.reply_text("I don't have access to the invite link!")
+
+GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args=True, filters=Filters.sudo_filter)
+
+dispatcher.add_handler(GETLINK_HANDLER)
