06a3127214df117dd0906a7f2cf61fa3a15bcc59
Sebastian <sebastiantaharuddin.personal@gmail.com>
2018-04-15 19:55:10 +0700

    Added to new module

--- tg_bot/modules/tidy.py
+++ tg_bot/modules/tidy.py
@@ -11,6 +11,8 @@ from tg_bot.modules.disable import DisableAbleCommandHandler
 def tidy(bot: Bot, update: Update):
     message = update.effective_message
     reply_text = re.sub(r'Pls', "Please", message.reply_to_message.text)
+    reply_text = re.sub(r'Plz', "Please", reply_text)
+    reply_text = re.sub(r'(sar|sur|Sar|Sur)', "sir", reply_text)
     message.reply_to_message.reply_text(reply_text)
 
 TIDY_HANDLER = DisableAbleCommandHandler("tidy", tidy)
