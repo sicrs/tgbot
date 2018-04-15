ad553b2d5c9d3af4d5f1a604de0f7c2307d8c4b7
Sebastian <sebastiantaharuddin.personal@gmail.com>
2018-04-15 19:38:34 +0700

    New module

--- tg_bot/modules/tidy.py
+++ tg_bot/modules/tidy.py
@@ -13,6 +13,6 @@ def tidy(bot: Bot, update: Update):
     reply_text = re.sub(r'Pls', "Please", message.reply_to_message.text)
     message.reply_to_message_.reply_text(reply_text)
 
-TIDY_HANDLER = DisableAbleCommandHandler("tidy". tidy)
+TIDY_HANDLER = DisableAbleCommandHandler("tidy", tidy)
 
 dispatcher.add_handler(TIDY_HANDLER)
