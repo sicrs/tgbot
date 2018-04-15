de27f3760d187b3d6e7273e709241c25782ab949
Sebastian <sebastiantaharuddin.personal@gmail.com>
2018-04-15 19:30:49 +0700

    New module

---
+++ tg_bot/modules/tidy.py
@@ -0,0 +1,18 @@
+import re
+
+from telegram import Message, Update, Bot, User
+from telegram import MessageEntity
+from telegram.ext import Filters, run_async
+
+from tgbot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+
+@run_async
+def tidy(bot: Bot, update: Update):
+    message = updater.effective_message
+    reply_text = re.sub(r'Pls', "Please", message.reply_to_message.text)
+    message.reply_to_message_.reply_text(reply_text)
+
+TIDY_HANDLER = DisableAbleCommandHandler("tidy". tidy)
+
+dispatcher.add_handler(TIDY_HANDLER)
